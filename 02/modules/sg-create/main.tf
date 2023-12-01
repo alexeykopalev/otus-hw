2c12d1d2aeb06a0b663fa18f3c2d54bf84980ac4
Alexey Kopalev <alexeykopalev@gmail.com>
2023-12-01 21:55:00 +0300

    add role ha-cluster

--- 02/modules/sg-create/main.tf
+++ 02/modules/sg-create/main.tf
@@ -46,6 +46,62 @@ resource "yandex_vpc_security_group" "prom-sg" {
     port              = 3260
   }
 
+    ingress {
+    protocol          = "TCP"
+    description       = "allow pcsd port"
+    v4_cidr_blocks    = ["10.10.1.0/24"]
+    port              = 2224
+  }
+
+      ingress {
+    protocol          = "TCP"
+    description       = "allow crmd port"
+    v4_cidr_blocks    = ["10.10.1.0/24"]
+    port              = 3121
+  }
+
+  ingress {
+    protocol          = "TCP"
+    description       = "allow corosync-qnetd"
+    v4_cidr_blocks    = ["10.10.1.0/24"]
+    port              = 5403
+  }
+
+  ingress {
+    protocol          = "UDP"
+    description       = "allow corosync multicast-udp"
+    v4_cidr_blocks    = ["10.10.1.0/24"]
+    port              = 5404
+  }
+
+  ingress {
+    protocol          = "UDP"
+    description       = "allow corosync"
+    v4_cidr_blocks    = ["10.10.1.0/24"]
+    port              = 5405
+  }
+
+  ingress {
+    protocol          = "TCP"
+    description       = "allow CLVM"
+    v4_cidr_blocks    = ["10.10.1.0/24"]
+    port              = 21064
+  }  
+
+  ingress {
+    protocol          = "TCP"
+    description       = "allow booth ticket manager"
+    v4_cidr_blocks    = ["10.10.1.0/24"]
+    port              = 9929
+  }
+
+  ingress {
+    protocol          = "UDP"
+    description       = "allow booth ticket manager"
+    v4_cidr_blocks    = ["10.10.1.0/24"]
+    port              = 9929
+  }
+
   ingress {
     protocol       = "ICMP"
     description    = "ping allow"
